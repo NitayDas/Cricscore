43266403b6545535c06f9cad22dc1d0bce789a17
Nitay_Das <nitaydas910@gmail.com>
2024-05-30 15:22:13 +0600

    changing view

--- match/views.py
+++ match/views.py
@@ -11,54 +11,15 @@ from datetime import datetime,timedelta
 import requests
 
 
-
 def home(request):
     return render(request, 'Home/home.html')
 
-# Storing OverSummary
-# def home(request):
-#     from .models import Matches,OverSummary
-    
-#     headers = {
-#         'X-RapidAPI-Key': 'b75aac835cmshaa98b93c54be468p128cc8jsn66c9ac71b9e8',
-#         'X-RapidAPI-Host': 'cricbuzz-cricket.p.rapidapi.com'
-#     }
-    
-#     matches = Matches.objects.exclude(state = 'Upcoming').exclude(state = 'Complete')
-    
-#     for match in matches:
-#         oversummary_url= f'https://cricbuzz-cricket.p.rapidapi.com/mcenter/v1/{match.match_id}/comm'
-#         # store_oversummary(oversummary_url,headers)
-    
-#     try:
-#         response= requests.get(oversummary_url, headers=headers)
-#         response.raise_for_status()
-        
-#         if response.status_code == 200:
-#             data = response.json()
-#             match_id = data.get('matchHeader', {}).get('matchId')
-#             commentary_list = data.get('commentaryList', [])
-            
-            
-#             for commentary in commentary_list:
-#                 OverSummary.objects.create(
-#                 match_id = match_id,
-#                 InningsId=str(commentary.get('inningsId', '')),
-#                 OverNum=str(commentary.get('overNumber', '')),
-#                 Event=commentary.get('event', ''),
-#                 commentary=commentary.get('commText', '')
-#                 )
-                    
-            
-#     except requests.RequestException as e:
-#         print(f'Failed to fetch oversummary from api: {oversummary_url}, Error: {e}')
-        
-#     return render(request, 'Home/home.html')
+
 
 class MatchesList(APIView):
     def get(self, request):
         today=timezone.now()
-        five_days_earlier = today - timedelta(days=7)
+        five_days_earlier = today - timedelta(days=8)
         seven_days_later = today + timedelta(days=7)
         matches=Matches.objects.filter(start_date__date__range=(five_days_earlier.date(),seven_days_later.date()))
         serializer = MatchesSerializer(matches,many=True)
