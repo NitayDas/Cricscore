673fa961fa19d2d5947de62bb7670f0a8fe16187
Nitay_Das <nitaydas910@gmail.com>
2025-04-05 03:38:52 +0600

    updated match

--- match/sentiment_analysis/scripts/test.py
+++
@@ -1,8 +0,0 @@
-import torch
-print(torch.cuda.is_available())  # Should print True if GPU is detected
-print(torch.cuda.device_count())  # Number of available GPUs
-print(torch.cuda.get_device_name(0))
-
-# import torch
-# torch.cuda.empty_cache()  # Clears cached memory
-# torch.cuda.ipc_collect()  # Frees up fragmented memory
