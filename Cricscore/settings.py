33323084f9abd9d5a369deb57837c2060854789e
Nitay Das <nitaydas910@gmail.com>
2025-04-09 16:45:20 +0600

    changes in Cricscore

--- Cricscore/settings.py
+++ Cricscore/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'corsheaders',
     'match',
-    'accounts',
+    'report',
     'django_celery_beat',
     'django_celery_results',
     'channels',
